6da82328f09d068f499dd82d789570089a53c788
fernando <fernandohds564@gmail.com>
2015-09-04 01:43:50 -0300

    implementation of impedance formulas in python3.

--- collective_effects.py
+++ collective_effects.py
@@ -2,10 +2,13 @@
 
 import numpy as np
 
+c = 299792458  # Velocity of light [m/s]
+
 class Element:
-    def __init__(self,name=None, folder=None, betax=None,betay=None):
-        self.name     = name
-        self.folder   = folder
+    def __init__(self,name=None, folder=None, betax=None,betay=None, quantity=1):
+        self.name     = name or ''
+        self.folder   = folder or os.path.abspath(os.path.curdir)
+        self.quantity = quantity
         self.betax    = betax or 7.2
         self.betay    = betay or 11.0
         self.w        = np.array([],dtype=float)
@@ -38,13 +41,16 @@ class Ring:
         self.version   = 'SI.v10.c01'
         self.circ      = 518.396
         self.T0        = self.circ/299792458
-        self.f0        = 1/self.T0
-        self.w0        = 2*np.pi*self.f0 # revolution angular frequency [Hz]
-        self.mom_cmpct = 1.7e-4       # momentum compaction factor
-        self.E         = 3e9          # energy [eV]
-        self.nuy       = 13.116
-        self.nux       = 48.131
-        self.harm_num  = 864           # harmonic Number
+        self.f0        = 1/self.T0  # revolution frequency [Hz]
+        self.w0        = 2*np.pi*self.f0 # revolution angular frequency [rad/s]
+        self.mom_cmpct = 1.7e-4    # momentum compaction factor
+        self.E         = 3e9       # energy [eV]
+        self.nuy       = 13.116    # vertical tune
+        self.nux       = 48.131    # horizontal tune
+        self.chromx    = 0.0       # horizontal chromaticity
+        self.chromy    = 0.0       # vertical chromaticity
+        self.harm_num  = 864       # harmonic Number
+        self.nbun      = 864       # number of bunches filled
 
         I = np.linspace(0,4,num=40)
         self.cur_bun   = I*1e-3
@@ -52,9 +58,9 @@ class Ring:
             self.phase       = 'Commissioning'
             self.nom_cur     = 0.100       # total current [A]
             self.nus         = 0.00435    # synchrotron tune
-            self.espread     = 7.64e-4 + 0*I
-            self.emitx       = 271e-12 + 0*I
-            self.emity       = 2.71e-12 + 0*I
+            self.espread     = lambda x:7.64e-4 +0*x
+            self.emitx       = lambda x:271e-12 +0*x
+            self.emity       = lambda x:2.71e-12+0*x
             self.damptx      = 17.1e-3
             self.dampty      = 22.7e-3
             self.dampte      = 13.6e-3
@@ -74,9 +80,9 @@ class Ring:
             self.phase       = 'Phase 2 with IDS'
             self.nom_cur     = 0.35        # total current [A]
             self.nus         = 0.00435    # synchrotron tune
-            self.espread     = 1e-02*(0.088704 + 0.015765*I -0.005477*I**2 + 0.0012452*I**3 -0.00011434*I**4)
-            self.emitx       = 1e-09*(0.18859  + 0.056781*I -0.015909*I**2 + 0.003445* I**3 -0.00031039*I**4)
-            self.emity       = 1e-12*(1.6497   + 1.0422*  I -0.51454 *I**2 + 0.14498*  I**3 -0.015059 * I**4)
+            self.espread     = lambda x:1e-2*(8.87e-2+1.58e-2*x-5.48e-3*x**2+1.25e-3*x**3-1.14e-4*x**4)
+            self.emitx       = lambda x:1e-9*(1.89e-1+5.61e-2*x-1.59e-2*x**2+3.44e-3*x**3-3.10e-4*x**4)
+            self.emity       = lambda x:1e-12*(1.6497+1.04220*x-5.15e-1*x**2+1.45e-1*x**3-1.51e-2*x**4)
             self.damptx      = 10.6e-3
             self.dampty      = 12.5e-3
             self.dampte      =  6.9e-3
@@ -85,16 +91,16 @@ class Ring:
             self.phase       = 'Phase 2 with IDS High Current'
             self.nom_cur     = 0.5        # total current [A]
             self.nus         = 0.00435    # synchrotron tune
-            self.espread     = 1e-02*(0.088704 + 0.015765*I -0.005477*I**2 + 0.0012452*I**3 -0.00011434*I**4)
-            self.emitx       = 1e-09*(0.18859  + 0.056781*I -0.015909*I**2 + 0.003445* I**3 -0.00031039*I**4)
-            self.emity       = 1e-12*(1.6497   + 1.0422*  I -0.51454 *I**2 + 0.14498*  I**3 -0.015059 * I**4)
+            self.espread     = lambda x:1e-2*(8.87e-2+1.58e-2*x-5.48e-3*x**2+1.25e-3*x**3-1.14e-4*x**4)
+            self.emitx       = lambda x:1e-9*(1.89e-1+5.68e-2*x-1.59e-2*x**2+3.45e-3*x**3-3.10e-4*x**4)
+            self.emity       = lambda x:1e-12*(1.6497+1.04220*x-5.15e-1*x**2+1.45e-1*x**3-1.51e-2*x**4)
             self.damptx      = 10.6e-3
             self.dampty      = 12.5e-3
             self.dampte      =  6.9e-3
             self.en_lost_rad = 829761.9 #eV
 
 
-    def loss_factor(w,Z,sigma,w0,nb):
+    def loss_factor(self,w,Z,sigma,w0,nb):
         # Calcula o loss factor and effective impedance para nb pacotes com
         # comprimento longitudinal sigma igualmente espacados.
         #
@@ -107,9 +113,6 @@ class Ring:
         #   sigma = tamanho longitudinal do feixe [m]
         #   w0 = frequencia angular de revolucao no anel [rad/s]
         #   nb = numero de pacotes preenchidos
-
-
-        c = 299792458
 
         pmin = np.ceil( w[0] /(w0*nb))  # arredonda em direcao a +infinito
         pmax = np.floor(w[-1]/(w0*nb)) # arredonda em direcao a -infinito
@@ -126,8 +129,6 @@ class Ring:
         # Calcula o kick factor para nb pacotes com comprimento longitudinal sigma
         # igualmente espacados.
 
-        c = 299792458
-
         pmin = np.ceil( w[0] /(w0*nb))  # arredonda em direcao a +infinito
         pmax = np.floor(w[-1]/(w0*nb)) # arredonda em direcao a -infinito
         p = np.arange(pmin, pmax+1)
@@ -140,11 +141,19 @@ class Ring:
         Zt_eff = sum(interpol_Z.*h)
         return nb*(w0/2/pi)*Zt_eff
 
-    def longitudinal_cbi(w, Zl, sigma, nb, w0, nus, eta, E, I_tot, m):
+    def longitudinal_cbi(self, w, Zl, sigma, m):
         # Calcula a impedancia longitudinal efetiva dos nb modos de oscilacao,
         # considerando um feixe gaussiano, para o modo azimutal m e radial k=0;
         # E calcula as instabilidades de Coupled_bunch a partir dela.
-        c = 299792458
+
+        nus  = self.nus
+        eta  = self.mom_cmpt
+        w0   = self.w0
+        eta  = self.mom_cmpct
+        E    = self.E
+        nb   = self.nbun
+        I_tot= self.nom_curr
+
         # Calculate Effective Impedance
         pmin = np.ceil( (w[ 0] -    m*nus*w0    )/(w0*nb)) #arredonda em direcao a +infinito
         pmax = np.floor((w[-1] - (nb-1+m*nus)*w0)/(w0*nb)) #arredonda em direcao a -infinito
@@ -159,14 +168,24 @@ class Ring:
         deltaw = 1j/(2*np.pi*2**m*np.math.factorial(m - 1)) * I_tot*eta/(E*nus*(sigma/c)**2) * Zl_eff
         return deltaw
 
-    def transverse_cbi(w,Z, sigma, nb, w0, nus, nut, chrom, eta, m, E, I_tot):
+    def transverse_cbi(w,Z, sigma, plane='y', chrom, m):
         # Calcula a impedância transversal efetiva dos nb modos de oscilacao,
         # considerando um feixe gaussiano, para o modo azimutal m e radial k=0;
         # E calcula as instabilidades de Coupled_bunch a partir dela.
         #
         # deltaw = transverse_cbi(w,Z, sigma, nb, w0, nus, nut, chrom, eta, m, E, I_tot)
 
-        c = 299792458
+        nus  = self.nus
+        eta  = self.mom_cmpt
+        w0   = self.w0
+        eta  = self.mom_cmpct
+        E    = self.E
+        nb   = self.nbun
+        I_tot= self.nom_curr
+        if plane.lower().startswith(('x','h')):
+            nut, chrom   = self.nux, self.chromx
+        else:
+            nut, chrom   = self.nuy, self.chromy
 
         ## Calculate Effective Impedance
         nucro = nut/eta*chrom
@@ -181,19 +200,19 @@ class Ring:
         interpol_Z = np.interp(wp,w,Z)
         Zt_eff = np.diag(interpol_Z * h)
 
-        ## Calculate Coupled_bunch Instabilities
+        ## Calculate Coupled_bunch Instability
 
         deltaw = -1j*I_tot/(4*np.pi*2**abs(m)*np.math.factorial(abs(m))) / E * w0 * Zt_eff
         return deltaw
 
-    def longitudinal_mode_coupling(w,Z, params):
+    def longitudinal_mode_coupling(self,w,Z, n_azi, n_rad):
 
         def bunch_spectrum(sigW,azi,rad):
             Inl = (1/np.sqrt(np.math.factorial(abs(azi)+rad) * np.math.factorial(rad)) *
                 sigW**(abs(azi)+2*rad) * np.exp(-sigW**2))
             return Inl
 
-        def calc_M(interpol_Z, wp, sigma, n_azi, n_rad):
+        def calc_M(interpol_Z, wp, sigma, n_azi=7, n_rad=6):
             sigW = wp*sigma/c/np.sqrt(2)
             A = np.zeros([1 + 2*n_azi, n_rad+1, 1 + 2*n_azi, n_rad+1])
             M = np.zeros([1 + 2*n_azi, n_rad+1, 1 + 2*n_azi, n_rad+1])
@@ -230,19 +249,14 @@ class Ring:
             return (A.reshape([(1 + 2*n_azi)*(1+n_rad),(1 + 2*n_azi)*(1+n_rad)]).transpose(),
                     M.reshape([(1 + 2*n_azi)*(1+n_rad),(1 + 2*n_azi)*(1+n_rad)]).transpose())
 
-
-        n_rad = params.n_rad;
-        n_azi = params.n_azi;
-        sigma = params.sigma;
-        I_b   = params.I;
-        E     = params.E;
-        w0    = params.w0;
-        nus   = params.nus;
-        eta   = params.eta;
-        nb    = params.nb;
-        mu    = params.mu;
-
-        c = 299792458;
+        I_b   = self.I
+        sigma = self.sigma(I_b)
+        E     = self.E
+        w0    = self.w0
+        nus   = self.nus
+        eta   = self.mom_cmpct
+        nb    = self.nbun
+        mu    = self.mu
 
         pmin = np.ceil( (w[0] -(mu + n_azi*nus)*w0)/(w0*nb)) # arredonda em direcao a +infinito
         pmax = np.floor((w[-1]-(mu + n_azi*nus)*w0)/(w0*nb)) # arredonda em direcao a -infinito
@@ -267,7 +281,7 @@ class Ring:
                 delta[:,ii] = np.linalg.eigvals(B)
 
 
-    def transverse_mode_coupling(w,Z, params):
+    def transverse_mode_coupling(self,w,Z, plane='y', n_azi=3, n_rad=4, mu=0):
 
         def bunch_spectrum(sigW,azi,rad):
             Inl = (1/np.sqrt(np.math.factorial(abs(azi)+rad) * np.math.factorial(rad)) *
@@ -311,21 +325,17 @@ class Ring:
             return (A.reshape([(1 + 2*n_azi)*(1+n_rad),(1 + 2*n_azi)*(1+n_rad)]).transpose(),
                     M.reshape([(1 + 2*n_azi)*(1+n_rad),(1 + 2*n_azi)*(1+n_rad)]).transpose())
 
-
-        n_rad = params.n_rad;
-        n_azi = params.n_azi;
-        sigma = params.sigma;
-        I_b   = params.I;
-        E     = params.E;
-        w0    = params.w0;
-        nus   = params.nus;
-        nut   = params.nut;
-        chrom = params.chrom;
-        eta   = params.eta;
-        nb    = params.nb;
-        mu    = params.mu;
-
-        c = 299792458;
+        I_b   = self.I
+        sigma = self.sigma(I)
+        E     = self.E
+        w0    = self.w0
+        nus   = self.nus
+        eta   = self.mom_cmpct
+        nb    = self.nbun
+        if plane.lower().startswith(('x','h')):
+            nut, chrom   = self.nux, self.chromx
+        else:
+            nut, chrom   = self.nuy, self.chromy
 
         nucro = nut/eta*chrom
         pmin = np.ceil( (w[0] -(mu + nut + n_azi*nus)*w0)/(w0*nb)) # arredonda em direcao a +infinito
@@ -335,7 +345,6 @@ class Ring:
         wp = w0*(p*nb + mu +nut + 1*nus)
         interpol_Z = np.interp(wp,w,Z)
         wpcro = wp - nucro*w0
-
 
         delta = np.zeros([(1 + 2*n_azi)*(1+n_rad), len(I_b)]);
         if len(sigma)~=1:
---
+++ impedances.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import scipy.special as scy
+
+c   = 299792458
+mu0 = 4*np.pi*1e-7
+ep0 = 1/c**2/mu0
+Z0  = mu0*c
+
+def longitudinal_resonator(Rs, Q, wr, w):
+    # Modelagem de impedância por soma de ressonadores.
+    # Inputs:
+    # Rs    = Vetor com as resistências Shunts [Ohm]
+    # Q     = Vetor com os fatores de qualidade
+    # wr    = Vetor com as frequências angulares de ressonância de cada oscilador [rad/s]
+    # w     = Frequências angulares em que sera calculada a impedância [rad/s]
+    #
+    # Outputs:
+    # Zl    = impedância longitudinal (real e imaginária) calculada para w
+    #
+    # Todos os outputs tem as dimensoes do SI
+    #
+    # Referência orginal:
+    #
+    # Chao, A., Physics of Collective Beam Instabilities in High Energy
+    # Accelerators, Wiley 1993.
+
+    Rs = Rs[:,None] # I am using broadcasting
+    Q  = Q[:,None]
+    wr = wr[:,None]
+
+    Zl = w*Rs / (w+1j*Q*(wr - w**2/wr))
+    return Zl.sum(0)
+
+def transverse_resonator(Rs, Q, wr, w):
+    # Modelagem de impedância por soma de ressonadores.
+    # Inputs:
+    # Rs    = Vetor com as resist??ncias Shunts [Ohm]
+    # Q     = Vetor com os fatores de qualidade
+    # wr    = Vetor com as frequ??ncias angulares de resson??ncia de cada oscilador [rad/s]
+    # w     = Frequ??ncias angulares em que sera calculada a imped??ncia [rad/s]
+    #
+    # Outputs:
+    # Zt    = imped??ncia transversal (real e imagin??ria) calculada para w
+    #
+    # Todos os outputs tem as dimens???es do SI
+    #
+    # Referência orginal:
+    #
+    # Chao, A., Physics of Collective Beam Instabilities in High Energy
+    # Accelerators, Wiley 1993.
+    Rs = Rs[:,None] # I am using broadcasting
+    Q  = Q[:,None]
+    wr = wr[:,None]
+
+    Zt = wr*Rs./(w + 1j*Q*(wr - w**2/wr)
+    return Zt.sum(0)
+
+def resistive_multilayer_round_pipe(w,epr,mur,b,L,E):
+
+    def Mtil(m, epr, mur, bet, nu, b):
+        def produto(A,B):
+            C = np.zeros((A.shape[0],B.shape[1]))
+            for i in range(A.shape[0]):
+                for j in range(B.shape[1]):
+                    for k in range(A.shape[1]):
+                        C[i,j,:] = C[i,j,:] + A[i,k,:]*B[k,j,:]
+            return C
+
+        for i in range(len(b)): # lembrando que length(b) = número de camadas - 1
+            x = nu[i+1,:] * b[i]
+            y = nu[i  ,:] * b[i]
+
+            if i<len(b):
+                D = np.zeros((4,4,nu.shape[1]))
+                z = nu[i+1,:]*b[i+1]
+                if not any(z.real<0):
+                    ind = (z.real<50)
+
+                    A = scy.iv(m,z[ind])
+                    B = scy.kn(m,z[ind])
+                    C = scy.iv(m,x[ind])
+                    E = scy.kn(m,x[ind])
+
+                    D[0,0,:] =  1
+                    D[1,1,ind]  = -B*C/(A*E)
+                    D[2,2,~ind] =  - np.exp(-2*(z[~ind]-x[~ind]))
+                    D[2,2,:] =  1;
+                    D[3,3,ind] = -B*C/(A*E)
+                    D[3,3,~ind] =  - np.exp(-2*(z[~ind]-x[~ind]))
+
+            Mt[0,0,:] = -nu[i+1,:]**2*b[i]/epr[i+1,:]*(
+                    epr[i+1,:]/nu[i+1,:]*(-scy.kve(m-1,x)/scy.kve(m,x) - m/x)
+                    - epr[i,:]/nu[i,:]  *( scy.ive(m-1,y)/scy.ive(m,y) - m/y))
+            Mt[0,1,:] = -nu[i+1,:]**2*b[i]/epr[i+1,:]*(
+                    epr[i+1,:]/nu[i+1,:]*(-scy.kve(m-1,x)/scy.kve(m,x) - m/x)
+                    - epr[i,:]/nu[i,:]  *(-scy.kve(m-1,y)/scy.kve(m,y) - m/y))
+            Mt[1,0,:] = -nu[i+1,:]**2*b[i]/epr[i+1,:]*(
+                    epr[i+1,:]/nu[i+1,:]*( scy.ive(m-1,x)/scy.ive(m,x) - m/x)
+                    - epr[i,:]/nu[i,:]  *( scy.ive(m-1,y)/scy.ive(m,y) - m/y))
+            Mt[1,1,:] = -nu[i+1,:]**2*b[i]/epr[i+1,:]*(
+                    epr[i+1,:]/nu[i+1,:]*( scy.ive(m-1,x)/scy.ive(m,x) - m/x)
+                    - epr[i,:]/nu[i,:]  *(-scy.kve(m-1,y)/scy.kve(m,y) - m/y))
+
+            Mt[0,2,:] = (nu[i+1,:]**2/nu[i,:]**2 - 1)*m/(bet*epr[i+1,:])
+            Mt[0,3,:] = Mt[0,2,:]
+            Mt[1,2,:] = Mt[0,2,:]
+            Mt[1,3,:] = Mt[0,2,:]
+            Mt[2,2,:] = -nu[i+1,:]**2*b[i]/mur[i+1,:]*(
+                    mur[i+1,:]/nu[i+1,:]*(-scy.kve(m-1,x)/scy.kve(m,x) - m/x)
+                    - mur[i,:]/nu[i,:]  *( scy.ive(m-1,y)/scy.ive(m,y) - m/y))
+            Mt[2,3,:] = -nu[i+1,:]**2*b[i]/mur[i+1,:]*(
+                    mur[i+1,:]/nu[i+1,:]*(-scy.kve(m-1,x)/scy.kve(m,x) - m/x)
+                    - mur[i,:]/nu[i,:]  *(-scy.kve(m-1,y)/scy.kve(m,y) - m/y))
+            Mt[3,2,:] = -nu[i+1,:]**2*b[i]/mur[i+1,:]*(
+                    mur[i+1,:]/nu[i+1,:]*( scy.ive(m-1,x)/scy.ive(m,x) - m/x)
+                    - mur[i,:]/nu[i,:]  *( scy.ive(m-1,y)/scy.ive(m,y) - m/y))
+            Mt[3,3,:] = -nu[i+1,:]**2*b[i]/mur[i+1,:]*(
+                    mur[i+1,:]/nu[i+1,:]*( scy.ive(m-1,x)/scy.ive(m,x) - m/x)
+                    - mur[i,:]/nu[i,:]  *(-scy.kve(m-1,y)/scy.kve(m,y) - m/y))
+            Mt[2,0,:] = (nu[i+1,:]**2/nu[i,:]**2 - 1)*m/(bet*mur[i+1,:])
+            Mt[2,1,:] = Mt[2,0,:]
+            Mt[3,0,:] = Mt[2,0,:]
+            Mt[3,1,:] = Mt[2,0,:]
+
+            if len(b) == 1:
+                M = Mt
+            else:
+                if (i ==1):
+                    M = produto(D,Mt)
+                elif i < len(b):
+                    M = produto(D,produto(Mt,M))
+                else:
+                    M = produto(Mt,M)
+
+    def alphaTM(m, epr, mur, bet, nu, b):
+        M = Mtil(m, epr, mur, bet, nu, b)
+
+        B = (M[0,1,:]*M[2,2,:] - M[2,1,:]*M[0,2,:]) / (M[0,0,:]*M[2,2,:] - M[0,2,:]*M[2,0,:])
+        alphaTM = spy.kn(m,nu[0,:]*b[0])/spy.iv(m,nu[0,:]*b[0]) * B
+        return alphaTM
+
+####################
+    gam = E/511e3
+    bet = sqrt(1-1/gam**2)
+    nu  = np.ones((epr.shape[0],1))*abs(w/c)*sqrt(1 - bet**2*epr.*mur)
+
+    Zl = 1j*L*w   /(2*np.pi*ep0 * (bet*c)**2*gam**2)*alphaTM(0, epr, mur, bet, nu, b)
+    Zv = 1j*L*w**2/(4*np.pi*ep0*c**2*(bet*c)*gam**4)*alphaTM(1, epr, mur, bet, nu, b)
+
+    # The code cant handle w = 0;
+    ind0 = np.where(w == 0)
+    if not ind0:
+        Zl[ind0] = Zl[ind0+1].real
+        Zv[ind0] = 0 + 1j*Zv[ind0+1].imag
+
+
+    # n=10;
+    # fil   = exp(-((-n:n)/(n/5)).^2)/sqrt(pi)/n*5;
+    # Zv = conv(Zv,fil,'same');
+    Zh = Zv
+
+    return Zl.conj(), Zv.conj(), Zh.conj()
+
+
+def lnls_calc_ferrite_kicker_impedance(w,a,b,d,L,epr,mur,Zg, model, coupled):
+    # Calculates Impedances for a ferrite kicker:
+    #       PIOR CASO:
+    #       multi-layer cilindrica com vacuo-coating(2microns)-ceramica-ferrite-PEC
+    #
+    #       MELHOR CASO:
+    #       multi-layer cilindrica com vacuo-coating(2microns)-ceramica-PEC
+    #
+    # Inputs:
+    #
+    # w   = vector of angular frequencies to evaluate impedances [rad/s]
+    # epr = vector with real and imaginary electric permeability of ferrite for
+    #       the frequency range of interest
+    # mur = vector with real and imaginary magnetic permissivity of ferrite for
+    #       the frequency range of interest
+    # L   = length of the structure [m]
+    # Zg  = Vector with generator impedance in function of w [Ohm]
+    #
+    # Outputs:
+    #
+    # Zl = Impedancia Longitudinal [Ohm]
+    # Zh = Impedancia Horizontal [Ohm/m]
+    # Zv = Impedancia Vertical   [Ohm/m]
+    #
+    #
+
+    epb     = [1 1                9.3            1      12   1]
+    mub     = [1 1                 1             1       1   1]
+    ange    = [0 0                 0             0       0   0]
+    angm    = [0 0                 0             0       0   0]
+    sigmadc = [0 2.4e6             1             1       1  5.9e7]
+    tau     = [0 0                 0             0       0   0]*27e-15
+    b1       = [(b - 2.0e-3 - 10e-6), (b - 2.0e-3), (b-1.0e-3), b , d]
+
+    for j = 1: length(epb)
+        epr1(j,:) = epb(j)*(1-1i.*sign(w).*tan(ange(j))) + sigmadc(j)./(1+1i*w*tau(j))./(1i*w*ep0)
+        mur1(j,:) = mub(j)*(1-1i.*sign(w).*tan(angm(j)))
+    end
+    epr1(5,:) = epr
+    mur1(5,:) = mur
+
+    if strcmp(model, 'tsutsui')
+        [Zl, Zh, Zv] = lnls_calc_impedance_tsutsui_model(w, epr, mur, a, b, d, L, 10)
+    elseif strcmp(model,'pior')
+        [Zl, Zv, Zh] = lnls_calc_impedance_multilayer_round_pipe(w, epr1, mur1, b1, L, 3)
+        Zv = pi^2/12*Zv
+        Zh = pi^2/24*Zh
+        Zqh = -Zh
+        Zqv = Zh
+    else
+        indx = [1 2 3 4 6]
+        mur1 = mur1(indx,:)
+        epr1 = epr1(indx,:)
+        b1    = b1([1 2 3 4])
+        [Zl, Zv, Zh] = lnls_calc_impedance_multilayer_round_pipe(w, epr1, mur1, b1, L, 3)
+        Zv = pi^2/12*Zv
+        Zh = pi^2/24*Zh
+        Zqh = -Zh
+        Zqv = Zh
+    end
+
+def kicker_coupled_flux(w,h,W,t,L,mur,Zg):
+    # Calculates Impedances for a ferrite kicker:
+    #   - For the Coupled Flux, it uses Davino-Hahn model.
+    #
+    #   DAVINO-HAHN MODEL:
+    #
+    #  #######################################    |
+    #  ###############FERRITE#################    t
+    #  #######################################    |
+    #  ###**                             **###  |
+    #  ###**  VACUUM                     **###  |      ______|  |_________|
+    #  ###**                             **###  |            |  |         |
+    #  ###**             +   .           **###  w            |  |         |
+    #  ###**                             **###  |            )||(         \
+    #  ###**             |_D_|           **###  |      Zk  L1)||(L2     Zg/
+    #  ###**                             **###  |            )||(         \
+    #  #######################################               | M|         /
+    #  #######################################               |  |         |
+    #  #######################################         ______|  |_________|
+    #      |______________h______________|
+    #
+    # Bibliografias:
+    #
+    # - Davino_D Hahn_H - Improved Analytical Model of the transverse coupling
+    #   impedance of ferrite kicker magnets - Phys. Rev. ST-AB v6 012001 2003
+    #
+    # - Nassibian G Sacherer F - Methods for measuring tranverse coupling
+    #   impedances in circular Accelerators - Nucl Inst and Meth. 159 21-27 1979
+
+    # Equivalent Circuit model.
+    D = 0.5e-3
+    M  = L*D*mu0/W
+    #     L2 = L*2*a*mu0/2/b
+    L2 = L*h*mu0/W*(mur*t/(mur*t+h*(h/W+1)))
+
+    Zk =      w * (M/L2)**2 * Zg*L2*1j/(1j*w*L2 + Zg)
+    Zx = c/D**2 * (M/L2)**2 * Zg*L2*1j/(1j*w*L2 + Zg)
+
+    return Zk.conj(), Zx.conj()  # take the conjugate to adapt impedance convention
+
+
+def kicker_tsutsui_model(w, epr, mur, a, b, d, L, n):
+    #   - For the Uncoupled Flux, we can choose between three models:
+    #
+    #       TSUTSUI MODEL:
+    #
+    #  ******************PEC**********************
+    #  *******************************************
+    #  **#######################################**      |
+    #  **################FERRITE################**      |
+    #  **#######################################**      |
+    #  **                                       **  |   d
+    #  **                                       **  b   |
+    #  **                                       **  |   |
+    #  **     VACUUM        .                   **  |   |
+    #  **                                       **
+    #  **                                       **
+    #  **                                       **
+    #  **#######################################**
+    #  **#######################################**
+    #  **#######################################**
+    #  *******************************************
+    #  *******************************************
+    #                       |__________a________|
+    #
+    # Inputs:
+    #
+    # w   = vector of angular frequencies to evaluate impedances [rad/s]
+    # epr = vector with real and imaginary electric permeability of ferrite for
+    #       the frequency range of interest
+    # mur = vector with real and imaginary magnetic permissivity of ferrite for
+    #       the frequency range of interest
+    # n   = max order of terms to sum
+    # L   = length of the structure [m]
+    #
+    # Outputs:
+    #
+    # Zl = Impedancia Longitudinal [Ohm]
+    # Zh = Impedancia Horizontal [Ohm/m]
+    # Zv = Impedancia Vertical   [Ohm/m]
+    #
+    # Bibliografias:
+    #
+    # - Tsutsui_H - Some Simplified Models of Ferrite Kicker Magnet for
+    #   Calculation of longitudinal Coupling Impedance - CERN-SL-2000-004
+    #
+    # - Tsutsui_H - Transverse Coupling Impedance of a Simplified Ferrite
+    #   Kicker Magnet Model - LHC Project Note 234 - 2000
+
+    # Valores do artigo do Wang et al para testar a implementacao das formulas
+    # do modelo do Tsutui.
+    # a = 103e-3
+    # b = 67e-3
+    # d = 122e-3
+    # L = 1.0
+    # Valores do artigo do Tsutsui para testar a implementacao das formulas
+    # a = 20e-3
+    # b = 16e-3
+    # d = 66e-3
+    # L = 0.6
+
+    # Terms for the infinite sum:
+    n = np.arange(0,n+1)
+
+    k = np.ones(n.shape)*w/c
+    epr = np.ones(n.shape)*epr
+    mur = np.ones(n.shape)*mur
+
+
+    kxn = np.repeats((2*n[:,None]+1)*np.pi/2/a, w.shape[0], axis=1)
+    kyn = np.sqrt((epr*mur-1)*k**2 - kxn**2)
+    sh  = np.sinh(kxn*b)
+    ch  = np.cosh(kxn*b)
+    tn  = np.tan(kyn*(b-d))
+    ct  = 1/np.tan(kyn*(b-d))
+
+    Zl = 1j*Z0*L/2/a / ((kxn/k*sh*ch*(1+epr*mur) + kyn/k*(mur*sh**2*tn - epr*ch**2*ct)
+        )/(epr*mur-1) - k/kxn*sh*ch)
+    Zl = Zl.sum(0)
+
+    Zv = 1j*Z0*L/2/a * kxn**2/k/(
+        (kxn/k*sh*ch*(1+epr*mur) + kyn/k*(mur*ch**2*tn - epr*sh**2*ct)
+        )/(epr*mur-1) - k/kxn*sh*ch)
+    Zv = Zv.sum(0)
+
+    Zh = 1j*Z0*L/2/a * kxn**2/k/(
+        (kxn/k*sh*ch*(1+epr.*mur) + kyn/k*(mur*sh**2*tn - epr*ch**2*ct)
+        )/(epr*mur-1) - k/kxn*sh*ch)
+    Zh = Zh.sum(0)
+
+    return Zl.conj(), Zh.conj(), Zv.conj() # impedance convention
