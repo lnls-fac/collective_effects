ac54b4ab8c294b965fcb0929b16d463cb301d7fd
Fernando <fernandohds564@gmail.com>
2017-08-11 07:39:02 -0300

    Change default executable to python-sirius link.

--- scripts/echo2d_submit.py
+++ scripts/echo2d_submit.py
@@ -1,28 +1,28 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python-sirius
 
 import argparse
 import subprocess as sp
 import psutil as psu
 
-def main(fname, modes=(0,1), nr_th=1):
 
+def main(fname, modes=(0, 1), nr_th=1):
     # Find the line to change in the reference file
     lines, ind = [], -1
-    with open(fname,'r') as f:
+    with open(fname, 'r') as f:
         lines = f.readlines()
-    for i,line in enumerate(lines):
+    for i, line in enumerate(lines):
         if line.strip().lower().startswith('modes='):
             ind = i
-    if ind<0:
+    if ind < 0:
         raise Exception('base file corrupted.')
 
     # Write the input files and submit calculations:
     procs = []
     for i in range(nr_th):
-        filename = '{0:s}_{1:02d}.txt'.format(fname.partition('.')[0],i)
+        filename = '{0:s}_{1:02d}.txt'.format(fname.partition('.')[0], i)
         str_modes = ' '.join(['{0:d}'.format(x) for x in modes[i::nr_th]])
         lines[ind] = 'Modes= {0:s}\n'.format(str_modes)
-        with open(filename,'w') as f:
+        with open(filename, 'w') as f:
             f.writelines(lines)
 
         procs.append(psu.Popen(['echo2d', filename], stdout=sp.PIPE))
@@ -32,19 +32,20 @@ def main(fname, modes=(0,1), nr_th=1):
         status = False
         for i, p in enumerate(procs):
             line = p.stdout.readline().decode()
-            print('Thread {0:2d} : {1:s}'.format(i,line),end='')
-            if p.poll() is not None: procs.remove(p)
+            print('Thread {0:2d} : {1:s}'.format(i, line), end='')
+            if p.poll() is not None:
+                procs.remove(p)
         print(30*'-')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('fileName',action='store', default='input_in.txt',
-        help='Name of the input file.',)
-    parser.add_argument('-m','--modes',action='store',nargs='+',
-        help='Modes to calculate the wake.', type=str)
-    parser.add_argument('-t','--threads',action='store',
-        help='Number of threads to use.', type=int)
+    parser.add_argument('fileName', action='store', default='input_in.txt',
+                        help='Name of the input file.',)
+    parser.add_argument('-m', '--modes', action='store', nargs='+',
+                        help='Modes to calculate the wake.', type=str)
+    parser.add_argument('-t', '--threads', action='store',
+                        help='Number of threads to use.', type=int)
 
     args = parser.parse_args()
 
@@ -54,16 +55,17 @@ if __name__ == '__main__':
         if len(ms) == 1:
             modes.append(int(ms[0]))
         elif len(ms) == 2:
-            modes.extend(  list( range(int(ms[0]), int(ms[1])) )  )
+            modes.extend(list(range(int(ms[0]), int(ms[1]))))
         elif len(ms) == 3:
-            modes.extend(  list( range(int(ms[0]), int(ms[1]), int(ms[2])) )  )
+            modes.extend(list(range(int(ms[0]), int(ms[1]), int(ms[2]))))
         else:
             raise Exception('Pattern not recognized')
     modes = sorted(modes)
-    modes = [x for i,x in enumerate(modes) if ( i==0 or (i>0 and x>modes[i-1]) )]
+    modes = [x for i, x in enumerate(modes)
+             if (i == 0 or (i > 0 and x > modes[i-1]))]
 
     print('Calulating Wake using echo2d for modes:')
     print(' '.join(['{0:2d}'.format(x) for x in modes]))
     print('Using {0:2d} processors'.format(args.threads))
 
-    main(args.fileName,modes=modes,nr_th=args.threads)
+    main(args.fileName, modes=modes, nr_th=args.threads)
--- scripts/ems-wake-analysis.py
+++ scripts/ems-wake-analysis.py
@@ -1,29 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python-sirius
 
 import optparse
 import os
 import pycolleff.process_wakes as ems
 
-def main(pth2sv,silent = False):
+
+def main(pth2sv, silent=False):
     simul_data = ems.EMSimulData()
-    ems.load_raw_data(simul_data,silent=silent)
-    ems.calc_impedance(simul_data,silent=silent,use_win='phase')
-    ems.save_processed_data(simul_data,silent=silent,pth2sv=pth2sv)
+    ems.load_raw_data(simul_data, silent=silent)
+    ems.calc_impedance(simul_data, silent=silent, use_win='phase')
+    ems.save_processed_data(simul_data, silent=silent, pth2sv=pth2sv)
     return simul_data
+
 
 if __name__ == '__main__':
 
     # configuration of the parser for the arguments
     parser = optparse.OptionParser()
-    parser.add_option('-p','--noplot',dest='plot',action='store_true',
+    parser.add_option('-p', '--noplot', dest='plot', action='store_true',
                       help="Show results", default=False)
-    parser.add_option('-s','--silent',dest='silent',action='store_true',
-              help="Print progress", default=False)
-    parser.add_option('-c','--calc',dest='calc',action='store_true',
+    parser.add_option('-s', '--silent', dest='silent', action='store_true',
+                      help="Print progress", default=False)
+    parser.add_option('-c', '--calc', dest='calc', action='store_true',
                       help="Calculate results", default=False)
-    parser.add_option('--pth2sv',dest='pth2sv',type='str',
+    parser.add_option('--pth2sv', dest='pth2sv', type='str',
                       help="Path to save the data.",
-                      default = 'analysis')
+                      default='analysis')
     (opts, _) = parser.parse_args()
 
     plot = not opts.plot
@@ -34,13 +36,14 @@ if __name__ == '__main__':
         simul_data = main(pth2sv, silent=silent)
         salva = True
     else:
-        fname  = os.path.sep.join([pth2sv,'SimulData.pickle'])
+        fname = os.path.sep.join([pth2sv, 'SimulData.pickle'])
         simul_data = ems.load_processed_data(fname)
         salva = False
 
-    ot = dict(save_figs=salva,pth2sv=pth2sv,show=False)
-    ems.plot_wakes(simul_data,**ot)
-    ems.plot_impedances(simul_data,**ot)
-    ems.plot_losskick_factors(simul_data,**ot)
+    ot = dict(save_figs=salva, pth2sv=pth2sv, show=False)
+    ems.plot_wakes(simul_data, **ot)
+    ems.plot_impedances(simul_data, **ot)
+    ems.plot_losskick_factors(simul_data, **ot)
     ems.create_make_fig_file(path=pth2sv)
-    if plot: ems.show_now()
+    if plot:
+        ems.show_now()
