ef4eff5fc65e834d38c6904915cbb8f2d5696998
Fernando <fernandohds564@gmail.com>
2017-08-29 22:09:00 -0300

    Create from_file and to_file methods for Results_t class.

--- cppcolleff/include/cppcolleff/Results.h
+++ cppcolleff/include/cppcolleff/Results.h
@@ -6,26 +6,34 @@
 
 class Results_t {
     private:
-        unsigned long calc_every, print_every, dump_every, nturns;
+        unsigned long calc_every, print_every, save_bunch_every, save_distributions_every, nturns;
         bool FB, Wd, Wl, Wq;
 
         bool calc_this_turn(const long n) const
         {
             if (calc_every == 0) return false;
             if ((n % calc_every)==0 || n==nturns) return true;
-            else return false;
+            return false;
         }
         bool print_this_turn(const long n) const
         {
             if (!calc_this_turn(n) || !print_in_screen || print_every == 0) return false;
             if ((n % (calc_every*print_every))==0 || n==nturns) return true;
-            else return false;
+            return false;
         }
-        bool dump_this_turn(const long n) const
+        bool save_bunch_this_turn(const long n) const
         {
-            if (!calc_this_turn(n) || !dump_bunch_to_file || dump_every == 0) return false;
-            if ((n % (calc_every*dump_every))==0 || n==nturns) return true;
-            else return false;
+            if (!save_bunch || save_bunch_every == 0) return false;
+            if ((n % save_bunch_every)==0 || n==nturns) return true;
+            return false;
+        }
+        bool save_distribution_this_turn(const long n) const
+        {
+            if (save_distributions_every == 0) return false;
+            if (!save_distribution_xx && !save_distribution_xx &&
+                !save_distribution_de && !save_distribution_ss) return false;
+            if ((n % save_distributions_every)==0 || n==nturns) return true;
+            return false;
         }
         void reserve_memory()
         {
@@ -38,7 +46,8 @@ class Results_t {
             if (Wl) Wlkick.reserve(np); else Wlkick.reserve(0);
         }
     public:
-        bool dump_bunch_to_file, print_in_screen;
+        bool save_bunch, print_in_screen, save_distribution_xx;
+        bool save_distribution_xl, save_distribution_de, save_distribution_ss;
         my_PartVector ave;
         my_PartVector std;
         my_Dvector Wlkick;
@@ -46,17 +55,20 @@ class Results_t {
         my_Dvector Wqkick;
         my_Dvector FBkick;
         Results_t (const unsigned long nt):
-            calc_every(1L), print_every(10L), dump_every(0L),
-            nturns(nt), dump_bunch_to_file(false), print_in_screen(true),
+            calc_every(1L), print_every(10L), save_bunch_every(0L), save_distributions_every(0L),
+            nturns(nt), save_bunch(false), print_in_screen(true), save_distribution_xx(false),
+            save_distribution_xl(false), save_distribution_de(false), save_distribution_ss(false),
             FB(false), Wd(false), Wq(false), Wl(false)      {reserve_memory();}
         Results_t (const unsigned long nt, const unsigned long eve):
-            calc_every(eve), print_every(10L), dump_every(0L),
-            nturns(nt), dump_bunch_to_file(false), print_in_screen(true),
+            calc_every(eve), print_every(10L), save_bunch_every(0L), save_distributions_every(0L),
+            nturns(nt), save_bunch(false), print_in_screen(true), save_distribution_xx(false),
+            save_distribution_xl(false), save_distribution_de(false), save_distribution_ss(false),
             FB(false), Wd(false), Wq(false), Wl(false)      {reserve_memory();}
         Results_t (const unsigned long nt, const unsigned long eve, const bool kicks):
-            calc_every(eve), print_every(10L), dump_every(0L),
-            nturns(nt), dump_bunch_to_file(false), print_in_screen(true),
-            FB(kicks), Wd(kicks), Wq(false), Wl(kicks)      {reserve_memory();}
+            calc_every(eve), print_every(10L), save_bunch_every(0L), save_distributions_every(0L),
+            nturns(nt), save_bunch(false), print_in_screen(true), save_distribution_xx(false),
+            save_distribution_xl(false), save_distribution_de(false), save_distribution_ss(false),
+            FB(kicks), Wd(kicks), Wq(kicks), Wl(kicks)      {reserve_memory();}
         ~Results_t() = default;
 
         void set_keepFB(const bool keep)    {FB = keep; reserve_memory();}
@@ -66,17 +78,21 @@ class Results_t {
         void set_nturns(const long nt){nturns = nt; reserve_memory();}
         void set_calc_every(const unsigned long eve){calc_every = eve; reserve_memory();}
         void set_print_every(const unsigned long eve){print_every = eve;}
-        void set_dump_to_file_every(const unsigned long eve){dump_every = eve;}
+        void set_save_bunch_every(const unsigned long eve){save_bunch_every = eve;}
+        void set_save_distributions_every(const unsigned long eve){save_distributions_every = eve;}
 
         unsigned long get_nturns() const {return nturns;}
         unsigned long get_calc_every() const {return calc_every;}
         unsigned long get_print_every() const {return print_every;}
-        unsigned long get_dump_every() const {return dump_every;}
+        unsigned long get_save_bunch_every() const {return save_bunch_every;}
+        unsigned long get_save_distributions_every() const {return save_distributions_every;}
         double calc_stats(const Bunch_t& bun, const long turn, ThreadPool& pool);
         double calc_stats(const Bunch_t& bun, const long turn);
         void register_Wkicks(const long turn, const my_Dvector& kik);
         void register_FBkick(const long turn, const double& kik);
-};
 
+        void to_file(const char* filename) const;
+        void from_file(const char* filename);
+};
 
 #endif
--- cppcolleff/src/Results.cpp
+++ cppcolleff/src/Results.cpp
@@ -65,9 +65,9 @@ double Results_t::calc_stats(
     rstd /= bun.num_part;
     rstd = sqrt(rstd - rave * rave);
 
-    if (dump_this_turn(turn)) {
+    if (save_bunch_this_turn(turn)) {
         char filename[50];
-        sprintf(filename,"turn%07lu.txt",turn);
+        sprintf(filename,"turn%07lu_bunch.txt",turn);
         bun.to_file(filename);
     }
     if (print_in_screen) {
@@ -99,4 +99,109 @@ void Results_t::register_Wkicks(const long turn, const my_Dvector& kik)
     if (Wl && calc_this_turn(turn)) {Wlkick.push_back(kik[0]);}
     if (Wd && calc_this_turn(turn)) {Wdkick.push_back(kik[1]);}
     if (Wq && calc_this_turn(turn)) {Wqkick.push_back(kik[2]);}
+}
+
+
+void Results_t::to_file(const char* filename) const
+{
+    auto g_bool = [](bool cond){return (cond) ? "true":"false";};
+
+	ofstream fp(filename);
+	if (fp.fail()) exit(1);
+	fp.setf(fp.left | fp.scientific);
+	fp.precision(15);
+    fp << setw(30) << "% number_of_turns" << nturns << endl;
+    fp << setw(30) << "% calc_statistics_every" << calc_every << " turns" << endl;
+    fp << setw(30) << "% print_in_screen" << g_bool(print_in_screen) << endl;
+    fp << setw(30) << "% print_in_screen_every" << print_every << " calcs" << endl;
+    fp << setw(30) << "% save_bunch" << g_bool(save_bunch) << endl;
+    fp << setw(30) << "% save_bunch_every" << save_bunch_every << " turns" << endl;
+    fp << setw(30) << "% save_distribution_xx" << g_bool(save_distribution_xx) << endl;
+    fp << setw(30) << "% save_distribution_xl" << g_bool(save_distribution_xl) << endl;
+    fp << setw(30) << "% save_distribution_de" << g_bool(save_distribution_de) << endl;
+    fp << setw(30) << "% save_distribution_ss" << g_bool(save_distribution_ss) << endl;
+    fp << setw(30) << "% save_distributions_every" << save_distributions_every << " turns" << endl;
+    fp << setw(30) << "% keep_feedback_kicks" << g_bool(FB) << endl;
+    fp << setw(30) << "% keep_wake_long_kicks" << g_bool(Wl) << endl;
+    fp << setw(30) << "% keep_wake_dipo_kicks" << g_bool(Wd) << endl;
+    fp << setw(30) << "% keep_wake_quad_kicks" << g_bool(Wq) << endl;
+    fp << setw(26) << "# <xx> [m]";
+    fp << setw(26) << "<xl>";
+    fp << setw(26) << "<de>";
+    fp << setw(26) << "<ss> [m]";
+    fp << setw(26) << "std(xx) [m]";
+    fp << setw(26) << "std(xl)";
+    fp << setw(26) << "std(de)";
+    fp << setw(26) << "std(ss) [m]";
+    if (Wl) fp << setw(26) << "Long Wake Kick";
+    if (Wd) fp << setw(26) << "Dipo Wake Kick";
+    if (Wq) fp << setw(26) << "Quad Wake Kick";
+    if (FB) fp << setw(26) << "Feedback Kick";
+    fp << endl;
+    fp.setf(fp.left | fp.showpos | fp.scientific);
+    for (auto i=0; i<ave.size(); ++i){
+        fp << setw(26) << ave[i].xx;
+        fp << setw(26) << ave[i].xl;
+        fp << setw(26) << ave[i].de;
+        fp << setw(26) << ave[i].ss;
+        fp << setw(26) << std[i].xx;
+        fp << setw(26) << std[i].xl;
+        fp << setw(26) << std[i].de;
+        fp << setw(26) << std[i].ss;
+        if (Wl) fp << setw(26) << Wlkick[i];
+        if (Wd) fp << setw(26) << Wdkick[i];
+        if (Wq) fp << setw(26) << Wqkick[i];
+        if (FB) fp << setw(26) << FBkick[i];
+        fp << endl;
+    }
+    fp.close();
+}
+
+void Results_t::from_file(const char* filename)
+{
+	ifstream fp(filename);
+	if (fp.fail()) return;
+
+    auto g_bool = [](string& cmd){return (cmd.compare("true")==0) ? true:false;};
+
+	double d1(0.0), d2(0.0), d3(0.0), d4(0.0);
+  	string line;
+	unsigned long line_count = 0;
+	while (getline(fp, line)) {
+  		line_count++;
+  		istringstream ss(line);
+		char c = ss.get();
+		while (c == ' ') c = ss.get();
+  		if (c == '#' || c == '\n') continue;
+  		else if (c == '%') {
+			string cmd;
+	  		ss >> cmd;
+            if (cmd.compare("number_of_turns") == 0){ss >> nturns; reserve_memory();}
+            else if (cmd.compare("calc_statistics_every") == 0){ss >> calc_every; reserve_memory();}
+            else if (cmd.compare("print_in_screen") == 0){ss >> cmd; print_in_screen = g_bool(cmd);}
+            else if (cmd.compare("print_in_screen_every") == 0){ss >> print_every;}
+            else if (cmd.compare("save_bunch") == 0){ss >> cmd; save_bunch = g_bool(cmd);}
+            else if (cmd.compare("save_bunch_every") == 0){ss >> save_bunch_every;}
+            else if (cmd.compare("save_distribution_xx") == 0){ss >> cmd; save_distribution_xx = g_bool(cmd);}
+            else if (cmd.compare("save_distribution_xl") == 0){ss >> cmd; save_distribution_xl = g_bool(cmd);}
+            else if (cmd.compare("save_distribution_de") == 0){ss >> cmd; save_distribution_de = g_bool(cmd);}
+            else if (cmd.compare("save_distribution_ss") == 0){ss >> cmd; save_distribution_ss = g_bool(cmd);}
+            else if (cmd.compare("save_distributions_every") == 0){ss >> save_distributions_every;}
+            else if (cmd.compare("keep_feedback_kicks") == 0){ss >> cmd; FB = g_bool(cmd); reserve_memory();}
+            else if (cmd.compare("keep_wake_long_kicks") == 0){ss >> cmd; Wl = g_bool(cmd); reserve_memory();}
+            else if (cmd.compare("keep_wake_dipo_kicks") == 0){ss >> cmd; Wd = g_bool(cmd); reserve_memory();}
+            else if (cmd.compare("keep_wake_quad_kicks") == 0){ss >> cmd; Wq = g_bool(cmd); reserve_memory();}
+            continue;
+  		}
+		ss.unget();
+        ss >> d1; ss >> d2; ss >> d3; ss >> d4;
+        ave.push_back(Particle_t(d1, d2, d3, d4));
+        ss >> d1; ss >> d2; ss >> d3; ss >> d4;
+        std.push_back(Particle_t(d1, d2, d3, d4));
+        if (Wl) {ss >> d1; Wlkick.push_back(d1);}
+        if (Wd) {ss >> d1; Wdkick.push_back(d1);}
+        if (Wq) {ss >> d1; Wqkick.push_back(d1);}
+        if (FB) {ss >> d1; FBkick.push_back(d1);}
+	}
+	fp.close();
 }
