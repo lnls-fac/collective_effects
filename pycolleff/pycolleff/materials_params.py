027824dc4f0c9f0cb43c3acb808f85c58d771661
Fernando <fernandohds564@gmail.com>
2018-01-22 13:30:12 -0200

    Create file with material electrodynamic properties.

---
+++ pycolleff/pycolleff/materials_params.py
@@ -0,0 +1,27 @@
+
+cu_cond     = 5.9e7
+cu_rel_time = 27e-15
+
+neg_cond     = 1e6
+neg_thick    = 1.0e-6
+neg_rel_time = 0.0
+
+ticoa_cond     = 1.8e6
+ticoa_thick    = 4e-6  # 10e-6 Is under study by Ricardo.
+ticoa_rel_time = 0
+
+fesi_cond = 2e6
+fesi_mub = 40
+
+ss_cond     = 1.3e6
+ss_rel_time = 0
+
+ndfe_cond = 1e6
+ndfe_mur  = 40
+
+cmd5005_epb = 12
+cmd5005_rho = 1
+cmd5005_mub = 1600
+cmd5005_ws = 20e6
+
+cer_epb = 9.3
