dc7ee0ba67931c82e0cdcd65f56c0cd906f30954
Murilo Alves <alvesb.murilo@gmail.com>
2018-07-17 11:02:48 -0300

    Simulate_landau changed to module and ring models to scrips

--- pycolleff/pycolleff/simulate_landau.py
+++ pycolleff/pycolleff/simulate_landau.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python-sirius
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-# import matplotlib.cm as cm
 import scipy.integrate as scy_int
 import mathphys as _mp
 import resource
-import sys
 
 c = _mp.constants.light_speed
 
@@ -36,7 +31,6 @@ class Ring:
         self.mom_cmpct = 0        # Momentum compaction factor
         self.en_spread = 0        # Relative Energy Spread
         self.peak_rf = 0          # Peak RF Voltage [V]
-        self.synch_tune = 0       # Synchrotron Tune
         self.peak_cur = 0         # Peak bunch current [A]
 
     @property
@@ -56,12 +50,14 @@ class Ring:
         return 2*np.pi/(self.T0)
 
     @property
+    def synch_tune(self):
+        return np.sqrt(self.mom_cmpct * self.peak_rf * self.wrf *
+                       np.abs(np.cos(self.synch_phase))
+                       / self.T0 / self.E0) / self.w0
+
+    @property
     def ws(self):
         return self.synch_tune*self.w0
-
-    @ws.setter
-    def ws(self, ws):
-        self.synch_tune = ws / self.w0
 
     @property
     def wrf(self):
@@ -71,10 +67,6 @@ class Ring:
     def bunlen(self):
         return self.en_spread * self.mom_cmpct * c / self.ws
 
-    @bunlen.setter
-    def bunlen(self, bl):
-        self.ws = self.en_spread * self.mom_cmpct * c / bl
-
     def get_Vrf(self, z):
         V0 = self.peak_rf
         phi0 = self.synch_phase
@@ -83,19 +75,14 @@ class Ring:
 
 
 class HarmCav:
-    def __init__(self, wrf, n, Q, Rs=None, r=0):
+    def __init__(self, wrf, n, Q, Rs=0, r=0):
         self._r = r
         self._wrf = wrf
         self.num = n
         self.quality_fact = Q
         self.psi = 0
         self.form_fact = 0
-        # self.harm_phase = 0
-
-        if Rs is not None:
-            self.shunt_imp = np.array(Rs)
-        else:
-            self.shunt_imp = 0
+        self.shunt_imp = Rs
 
     @property
     def delta(self):
@@ -114,9 +101,22 @@ class HarmCav:
         self.psi = np.pi/2 - self.num*phih
 
     @property
+    def dw(self):
+        return self.num * self._wrf - self.wr
+
+    @dw.setter
+    def dw(self, dw):
+        self.wr = self.num * self._wrf - dw
+
+    @property
     def wr(self):
         delta = self.delta
         return self.num*self._wrf*(-delta + np.sqrt(delta*delta + 1))
+
+    @wr.setter
+    def wr(self, wr):
+        x = wr / self.num / self._wrf
+        self.delta = 1 / 2 * (x - 1 / x)
 
     @property
     def alpha(self):
@@ -151,12 +151,12 @@ class HarmCav:
 
     def calc_flat_potential(self, ring, F=1):
         It = ring.nom_cur
-        self.psi = np.pi - np.abs(np.arccos(self.k * ring.peak_rf / 2 / It /
-                                            self.shunt_imp/F))
-        # self.harm_phase = (1/self.num)*np.arctan(-self.num*self._r /
-        #                                          (np.sqrt((self.num**2-1)**2 -
-        #                                           self.num**4*self._r**2)))
-        # self.psi = np.pi/2 + self.num * self.harm_phase
+        self.harm_phase = (1/self.num)*np.arctan(-self.num*self._r /
+                                                 (np.sqrt((self.num**2-1)**2 -
+                                                  self.num**4*self._r**2)))
+        self.psi = np.pi/2 - self.num * self.harm_phase
+        # self.psi = np.pi - np.abs(np.arccos(self.k * ring.peak_rf / 2 / It /
+                                            # self.shunt_imp/F))
         self.form_fact = F
 
     def print_param(self, ring):
@@ -171,9 +171,9 @@ class HarmCav:
         print('Harm. cav. phase {0:7.3f}°'.format(self.harm_phase * 180/np.pi))
         print('Shunt Impedance {0:6.4f}M'.format(self.shunt_imp*1e-6))
         print('Form factor {0:6.4f}'.format(self.form_fact))
-        print('Harm. cav. detuning {0:7.3f}kHz'.format(self.delta * self.wr / 2
+        print('Harm. cav. detuning {0:7.3f}kHz'.format(self.dw / 2
                                                        / np.pi*1e-3))
-        print('Harm. cav. detuning {0:7.3f}º'.format(self.psi * 180 / np.pi))
+        print('Harm. cav. psi {0:7.3f}º'.format(self.psi * 180 / np.pi))
 
 
 class Lambda:
@@ -199,27 +199,11 @@ class Lambda:
             V = V[None, :]
         sigE = ring.en_spread
         a = ring.mom_cmpct
-        pos = np.array(self.z)
-        npo = len(pos)//2
 
-        pos_par = np.delete(pos, npo)
-        z_minus = np.split(pos_par, 2)[0]
-        z_minus = np.append(z_minus, 0)
-        z_plus = np.split(pos_par, 2)[1]
-        z_plus = np.append(0, z_plus)
+        U = -scy_int.cumtrapz(V, self.z, initial=0)
+        U -= np.min(U, axis=1)[:, None]
 
-        # U = -scy_int.cumtrapz(V, self.z, initial=0)
-        # U -= np.min(U, axis=1)[:, None]
-        # rho = np.exp(-U/sigE/sigE/a/ring.C0/2)
-        # rho /= np.trapz(rho, self.z)[:, None]
-
-        U_minus = -scy_int.cumtrapz(V[:, :npo+1], z_minus, initial=0)
-        U_plus = -scy_int.cumtrapz(V[:, npo:], z_plus, initial=0)
-        U_plus += U_minus[:, -1][:, None]
-        U = np.concatenate((U_minus, U_plus), axis=1)
-        U_new = np.delete(U, npo, axis=1)
-
-        rho = np.exp(-U_new/sigE/sigE/a/ring.C0)
+        rho = np.exp(-U/sigE/sigE/a/ring.C0)
         rho /= np.trapz(rho, self.z)[:, None]
         return rho
 
@@ -287,8 +271,8 @@ def get_potentials_imp(ring, harm_cav, lamb, n_terms=5):
 
     for p in range(p0-n_terms, p0+n_terms+1):
         wp = p*w0
-        B = np.exp(-1j*wp*dif*C0/h/c)
-        lamb_w = cur*np.trapz(lamb.dist*np.exp(-1j*wp*z/c)[None, :], z)
+        B = np.exp(-1j * wp * dif * C0 / h / c)
+        lamb_w = cur * np.trapz(lamb.dist*np.exp(-1j * wp * z / c)[None, :], z)
         V = np.dot(B, lamb_w)
         Zp = Rs/(1 - 1j * Q * (wr / wp - wp / wr))
         V_hat = (w0 / 2 / np.pi * Zp * np.exp(1j*wp*z/c))[None, :] * V[:, None]
@@ -306,288 +290,3 @@ def get_potential_analytic_uni_fill(ring, harm_cav, z):
     wrf = ring.wrf
     z_n = z - z0
     return -2*It*Rs*F*np.cos(psi)*np.cos(n*wrf*z_n/c-psi)/ring.E0
-
-
-def main():
-
-    ring = Ring()
-
-    # # Sirius
-    # ring.E0 = 3e9
-    # ring.nom_cur = 350e-3
-    # ring.en_lost_rad = 872.7e3
-    # ring.frf = 499.658e6
-    # ring.peak_rf = 3e6
-    # ring.harm_num = 864
-    # # ring.bunlen = 2.5e-3
-    # ring.en_spread = 8.5e-4
-    # ring.mom_cmpct = 1.639e-4
-    # ring.synch_tune = 4.644e-3
-
-    # # MAX IV
-    # ring.E0 = 3e9
-    # ring.nom_cur = 500e-3
-    # ring.en_lost_rad = 856e3
-    # ring.frf = 99.931e6
-    # ring.peak_rf = 1.63e6
-    # ring.harm_num = 176
-    # # ring.bunlen = 10.1e-3
-    # ring.en_spread = 7.82e-4
-    # ring.mom_cmpct = 3.07e-4
-    # ring.synch_tune = 1.994e-3
-
-    # MAX IV without ID
-    ring.E0 = 3e9
-    ring.nom_cur = 500e-3
-    ring.en_lost_rad = 360e3
-    ring.frf = 99.931e6
-    ring.peak_rf = 1.02e6
-    ring.harm_num = 176
-    ring.bunlen = 40e-12*c
-    ring.en_spread = 7.82e-4
-    ring.mom_cmpct = 3.07e-4
-    ring.synch_tune = 1.994e-3
-    df = -48.1227e3
-
-    # # MAX II
-    # ring.E0 = 1.5e9
-    # ring.en_lost_rad = 133.4e3
-    # ring.mom_cmpct = 3.82e-3
-    # ring.en_spread = 7.01e-4
-    # ring.harm_num = 30
-    # ring.nom_cur = 48.75e-3
-    # ring.peak_rf = 399.023e3
-    # ring.frf = 99.959e6
-    # ring.ws = 2*np.pi*7125
-    # df = -26.370e3
-
-    # # SLS
-    # ring.E0 = 2.4e9
-    # ring.en_spread = 9e-4
-    # ring.frf = 499.632e6
-    # ring.harm_num = 480
-    # ring.mom_cmpct = 6e-4
-    # ring.en_lost_rad = 530e3
-    # ring.peak_rf = 2.1e6
-    # ring.nom_cur = 400e-3
-    # # ring.bunlen = 4e-3
-    # ring.ws = 2*np.pi*7.5e3
-    # df = -48.1227e3
-
-    # # SSRF
-    # ring.E0 = 3.5e9
-    # ring.en_spread = 9.8e-4
-    # ring.frf = 500e6
-    # ring.harm_num = 720
-    # ring.mom_cmpct = 4.2e-4
-    # ring.en_lost_rad = 1.44e6
-    # ring.peak_rf = 4e6
-    # ring.nom_cur = 300e-3
-    # ring.bunlen = 3.8e-3
-    # df = -63e3
-
-    r = ring.en_lost_rad/ring.peak_rf
-
-    wrf = ring.wrf
-    krf = wrf/c
-    h = ring.harm_num
-    # It = ring.nom_cur
-    It = ring.nom_cur
-    # Q = 2.6e8
-    # Rs = 88*Q
-    # hc = HarmCav(wrf, n=3, Q=Q, Rs=Rs, r=r)  # NSLS-2 SC-3HC
-    # hc = HarmCav(wrf, n=3, Q=21600, Rs=2.017e6, r=r)  # MAX IV
-    hc = HarmCav(wrf, n=3, Q=21600, Rs=2.36441e6, r=r)  # MAX IV without IDs
-    # hc = HarmCav(wrf, n=5, Q=21720, Rs=1.57e6, r=r)  # MAX II
-    # hc = HarmCav(wrf, n=3, Q=2e8, Rs=88.4*2e8, r=r)  # SLS
-    F = np.exp(-(ring.bunlen*hc.num*wrf/c)**2)
-    # F = 0.948882
-
-    hc.calc_flat_potential(ring, F=F)
-
-    fr = hc.num*ring.frf - df
-    hc.delta = df/fr
-
-    # hc.psi = 90.003641*np.pi/180  # Flat condition to Rs = 88Q
-    # hc.psi = 99.888555*np.pi/180  # Flat condition to Rs = 8.48M
-
-    # hc.shunt_imp = 2.017e6
-    # hc.psi = 103.717*np.pi/180
-
-    # hc.shunt_imp = 4.2e6
-    # hc.psi = 96.558*np.pi/180
-
-    # If the detuning is given in frequency
-    # fr = hc.num*ring.frf - df
-    # hc.delta = -(hc.num*ring.frf / fr - 1)
-    # hc.wr = 2*np.pi*fr
-
-    z0 = hc.length_shift
-    sigz = ring.bunlen
-
-    zlim = 30*sigz
-    npoints = 1501
-    z = np.linspace(-zlim, zlim, npoints)
-
-    Ib = np.zeros(h, dtype=float)
-    nfill = h
-    start = 0
-    Ib[start:start+nfill] = It/nfill
-
-    lamb = Lambda(z, Ib, ring)
-
-    Vrf = ring.get_Vrf(z)
-    dist_old = lamb.get_gauss_dist(z0, sigz)
-    lamb.dist = np.array(dist_old)
-
-    V_i = get_potentials_imp(ring, hc, lamb)
-    Vt_imp = Vrf[None, :] + V_i
-    dist_old = lamb.get_dist(Vt_imp, ring)
-    F = np.trapz(dist_old*np.exp(1j*hc.num*wrf*z/c), z)
-    F /= np.trapz(dist_old, z)
-    F_mod = np.absolute(F[0])
-
-    # V_w = get_potentials_wake(ring, hc, lamb)
-    # Vt_wake = Vrf[None, :] + V_w
-    # dist_old = lamb.get_dist(Vt_wake, ring)
-    # F = np.trapz(dist_old*np.exp(1j*hc.num*wrf*z/c), z)
-    # F /= np.trapz(lamb.dist, z)
-    # F_mod = np.absolute(F[0])
-
-    epsilon = 1e-10
-    param_conv = 15
-    n_iters = 5000
-
-    # plt.plot(lamb.cur*1e3)
-
-    plt.figure(figsize=(10, 14))
-    gs = gridspec.GridSpec(3, 1)
-    gs.update(
-        left=0.10, right=0.95, bottom=0.10,
-        top=0.97, wspace=0.35, hspace=0.25)
-    ax1 = plt.subplot(gs[0, 0])
-    ax2 = plt.subplot(gs[1, 0], sharex=ax1)
-    ax3 = plt.subplot(gs[2, 0])
-    ph = z*krf
-    # ph0 = z0*krf
-
-    dist_new = np.zeros(dist_old.shape)
-    # cmap = cm.brg(np.linspace(0, 1, n_iters+1))
-    for i in range(n_iters):
-        lamb.dist = np.array(dist_old)
-        lamb.form_fact = np.array(F)
-        F_new = np.trapz(lamb.dist*np.exp(1j*hc.num*wrf*z/c), z)
-        F_new /= np.trapz(lamb.dist, z)
-        F_mod = np.absolute(F_new[0])
-
-        # hc.calc_flat_potential(ring, F=F_mod)
-
-        V_i = get_potentials_imp(ring, hc, lamb)
-        Vt_imp = Vrf[None, :] + V_i
-        dist_new = lamb.get_dist(Vt_imp, ring)
-
-        # V_w = get_potentials_wake(ring, hc, lamb)
-        # Vt_wake = Vrf[None, :] + V_w
-        # dist_new = lamb.get_dist(Vt_wake, ring)
-
-        dif = dist_new - dist_old
-        res = np.trapz(dif * dif, z)
-        conv = np.sqrt(np.max(res))
-        print('{0:03d}: {1:f}, F: {2:f}, Psi: {3:f}'.format(i, conv, F_mod,
-                                                            hc.psi*180/np.pi))
-        if conv < epsilon:
-            break
-
-        dist_old *= param_conv
-        dist_old += dist_new
-        dist_old /= param_conv + 1
-        # lab = ''
-        # if not i % 10:
-        #     lab = '{0:02d}'.format(i)
-        #     ax1.plot(ph, Vt_imp[0, :], color=cmap[i], label=lab)
-        #     ax2.plot(ph, dist_new[0, :], color=cmap[i], label=lab)
-
-    lamb.dist = np.array(dist_new)
-    sigma_z_imp = lamb.get_bun_lens()
-    bl_imp = np.mean(sigma_z_imp)
-    z_ave_i = lamb.get_synch_phases()
-    z_ave_ave_i = np.mean(z_ave_i)
-
-    print('IMPEDANCE')
-    hc.print_param(ring)
-
-    V_i = get_potentials_imp(ring, hc, lamb)
-    Vt_imp = Vrf + V_i[0, :]
-
-    # V_w = get_potentials_wake(ring, hc, lamb)
-    # Vt_wake = Vrf + V_w[0, :]
-
-    # fwhm = 2*np.sqrt(2*np.log(2))
-
-    print('sync phase: {0:7.3f} mm'.format(z_ave_ave_i*1e3))
-    print('bun length: {0:7.3f} mm ({1:7.3f} ps)'.format(bl_imp*1e3,
-                                                         bl_imp*1e12/c))
-    print('=============================')
-
-    print('ANALYTICAL')
-
-    # hc.shunt_imp = 2.017e6
-    # hc.psi = 103.717*np.pi/180
-    hc.form_fact = F_mod
-
-    V_a = get_potential_analytic_uni_fill(ring, hc, z)
-    Vt_a = Vrf + V_a
-
-    lamb.dist = lamb.get_dist(Vt_a, ring)
-    sigma_z_a = lamb.get_bun_lens()
-    bl_a = np.mean(sigma_z_a)
-    z_ave_a = lamb.get_synch_phases()
-    z_ave_ave_a = np.mean(z_ave_a)
-
-    print('sync phase analy: {0:7.3f}'.format(z_ave_ave_a*1e3))
-    print('bun length analy: {0:7.3f} mm ({1:7.3f} ps)'.format(bl_a*1e3,
-                                                               bl_a*1e12/c))
-
-    ax1.plot(ph, Vt_imp, label='Final Imp')
-    # ax1.plot(ph, Vt_wake, label='Final Wake')
-    ax1.plot(ph, Vt_a, label='Final Analytic')
-
-    ax2.plot(ph, dist_new[0, :], '--', label='Final Imp - 0')
-    ax2.plot(ph, lamb.dist[0, :], label='Final Analytic')
-
-    ax3.plot(sigma_z_imp*1e3, label='Bunch Length - Imp')
-    # ax3.plot(z_ave_i*1e3, label='Synch Phase - Imp')
-    # ax3.plot(sigma_z_a*1e3, label='Bunch Length - Analytic')
-    # ax3.plot(z_ave_a*1e3, label='Synch Phase - Analytic')
-
-    # ax1.set_xlim([-0.5, 0.5])
-    # ax1.set_ylim([-5e-7, 5e-7])
-    ax1.legend(loc='best')
-    ax2.legend(loc='best')
-    ax3.legend(loc='best')
-    ax1.grid(True)
-    ax2.grid(True)
-    ax3.grid(True)
-    plt.show()
-
-    # fig2 = plt.figure()
-    # fig2.suptitle('Potencial de Equilíbrio', fontsize=14, fontweight='bold')
-    # ax = fig2.add_subplot(111)
-    # ax.plot(ph, Vt_imp, label='Total')
-    # ax.plot(ph, V_i[0, :], label='3HC')
-    # ax.plot(ph, Vrf, label='RF Principal')
-    # # ax.plot(ph, lamb.dist[0, :], label='Analítico')
-    # fig2.subplots_adjust(top=0.85)
-    # ax.set_xlabel('Fase [rad]')
-    # ax.legend(loc='best')
-    # ax.grid(True)
-    # plt.show()
-
-
-if __name__ == "__main__":
-    memory_limit()   # Limitates maximun memory usage to half
-    try:
-        main()
-    except MemoryError:
-        sys.stderr.write('\n\nERROR: Memory Exception\n')
-sys.exit(1)
---
+++ pycolleff/pycolleff/ssrf.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python-sirius
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import sys
+import mathphys as _mp
+import simulate_landau as landau_cav
+
+c = _mp.constants.light_speed
+
+
+def main():
+
+    ring = landau_cav.Ring()
+
+    # SSRF
+    ring.E0 = 3.5e9
+    ring.en_spread = 9.8e-4
+    ring.frf = 500e6
+    ring.harm_num = 720
+    ring.mom_cmpct = 4.2e-4
+    ring.en_lost_rad = 1.44e6
+    ring.peak_rf = 4.8e6
+    ring.nom_cur = 300e-3
+
+    r = ring.en_lost_rad/ring.peak_rf
+
+    wrf = ring.wrf
+    krf = wrf/c
+    h = ring.harm_num
+    It = ring.nom_cur
+    sigz = ring.bunlen
+
+    Q = 1.5e8
+    hc = landau_cav.HarmCav(wrf, n=3, Q=Q, Rs=180*Q, r=r)  # SSRF
+
+    F = np.exp(-(sigz*hc.num*wrf/c)**2)
+
+    hc.calc_flat_potential(ring, F=F)
+
+    hc.dw = 57e3 * 2 * np.pi
+
+    zlim = 30*sigz
+    npoints = 1501
+    z = np.linspace(-zlim, zlim, npoints)
+
+    epsilon = 1e-5
+    param_conv = 10
+    n_iters = 5000
+
+    Ib = np.zeros(h, dtype=float)
+    s_fill = h
+    n_trains = 1
+    s_gap = (h - n_trains * s_fill) // n_trains
+    Ib[:] = It / s_fill / n_trains
+
+    for j in range(n_trains):
+        Ib[j * (s_fill + s_gap) + s_fill:(j + 1) * (s_fill+s_gap)] = 0
+
+    lamb = landau_cav.Lambda(z, Ib, ring)
+
+    Vrf = ring.get_Vrf(z)
+    dist_old = lamb.get_gauss_dist(hc.length_shift, sigz)
+    lamb.dist = np.array(dist_old)
+
+    V_i = landau_cav.get_potentials_imp(ring, hc, lamb)
+    Vt_imp = Vrf[None, :] + V_i
+    dist_old = lamb.get_dist(Vt_imp, ring)
+
+    # V_w = get_potentials_wake(ring, hc, lamb)
+    # Vt_wake = Vrf[None, :] + V_w
+    # dist_old = lamb.get_dist(Vt_wake, ring)
+
+    plt.figure(figsize=(10, 14))
+    gs = gridspec.GridSpec(4, 1)
+    gs.update(left=0.10, right=0.95, bottom=0.10,
+              top=0.97, wspace=0.35, hspace=0.25)
+    ax1 = plt.subplot(gs[0, 0])
+    ax2 = plt.subplot(gs[1, 0])
+    ax3 = plt.subplot(gs[2, 0], sharex=ax2)
+    ax4 = plt.subplot(gs[3, 0], sharex=ax2)
+
+    dist_new = np.zeros(dist_old.shape)
+
+    for i in range(n_iters):
+        lamb.dist = np.array(dist_old)
+        lamb.form_fact = np.array(F)
+        F_new = np.trapz(lamb.dist*np.exp(1j*hc.num*wrf*z/c), z)
+        F_new /= np.trapz(lamb.dist, z)
+        F_mod = np.absolute(np.mean(F_new))
+
+        V_i = landau_cav.get_potentials_imp(ring, hc, lamb)
+        Vt_imp = Vrf[None, :] + V_i
+        dist_new = lamb.get_dist(Vt_imp, ring)
+
+        # V_w = get_potentials_wake(ring, hc, lamb)
+        # Vt_wake = Vrf[None, :] + V_w
+        # dist_new = lamb.get_dist(Vt_wake, ring)
+
+        dif = dist_new - dist_old
+        res = np.trapz(np.absolute(dif), z)
+        conv = np.mean(res)
+        print('{0:03d}: {1:f}, F: {2:f}'.format(i, conv, F_mod))
+
+        if conv < epsilon:
+            break
+
+        dist_old *= param_conv
+        dist_old += dist_new
+        dist_old /= param_conv + 1
+
+    lamb.dist = np.array(dist_new)
+    sigma_z_imp = lamb.get_bun_lens()
+    bl_imp = np.mean(sigma_z_imp)
+    z_ave_i = lamb.get_synch_phases()
+    z_ave_ave_i = np.mean(z_ave_i)
+
+    print('IMPEDANCE')
+    hc.print_param(ring)
+
+    V_i = landau_cav.get_potentials_imp(ring, hc, lamb)
+    Vt_imp = Vrf + V_i[0, :]
+
+    # V_w = get_potentials_wake(ring, hc, lamb)
+    # Vt_wake = Vrf + V_w[0, :]
+
+    # fwhm = 2*np.sqrt(2*np.log(2))   # To calculate FWHM Bunch Length for Gaussian distribution
+
+    print('sync phase: {0:7.3f} mm'.format(z_ave_ave_i*1e3))
+    print('bun length: {0:7.3f} mm ({1:7.3f} ps)'.format(bl_imp*1e3,
+                                                         bl_imp*1e12/c))
+    print('=============================')
+
+    # print('ANALYTICAL')
+    #
+    # # hc.shunt_imp = 2.017e6
+    # # hc.psi = 103.717*np.pi/180
+    # hc.form_fact = F_mod
+    #
+    # V_a = get_potential_analytic_uni_fill(ring, hc, z)
+    # Vt_a = Vrf + V_a
+    #
+    # lamb.dist = lamb.get_dist(Vt_a, ring)
+    # sigma_z_a = lamb.get_bun_lens()
+    # bl_a = np.mean(sigma_z_a)
+    # z_ave_a = lamb.get_synch_phases()
+    # z_ave_ave_a = np.mean(z_ave_a)
+    #
+    # print('sync phase analy: {0:7.3f}'.format(z_ave_ave_a*1e3))
+    # print('bun length analy: {0:7.3f} mm ({1:7.3f} ps)'.format(bl_a*1e3,
+    #                                                            bl_a*1e12/c))
+
+    ph = z*krf
+    ax1.plot(ph, dist_new[0, :], label='Distribution 1st bunch')
+    ax2.plot(lamb.cur, label='Current')
+
+    mask = lamb.cur < 1e-5
+    sigma_z_imp[mask] = np.nan
+    z_ave_i[mask] = np.nan
+
+    ax3.plot(sigma_z_imp/ring.bunlen, label='Bunch Lengthening factor')
+    ax4.plot(ring.synch_phase - (np.pi/2 + z_ave_i*krf), label='Synch Phase')
+
+    ax1.legend(loc='best')
+    ax2.legend(loc='best')
+    ax3.legend(loc='best')
+    ax4.legend(loc='best')
+    ax1.grid(True)
+    ax2.grid(True)
+    ax3.grid(True)
+    ax4.grid(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    landau_cav.memory_limit()   # Limitates maximum memory usage to half
+    try:
+        main()
+    except MemoryError:
+        sys.stderr.write('\n\nERROR: Memory Exception\n')
+sys.exit(1)
