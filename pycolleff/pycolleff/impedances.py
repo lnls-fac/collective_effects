02c2007952f7e94a59050398a327a4bd4b3eb34f
Fernando <fernandohds564@gmail.com>
2022-03-17 10:34:21 -0300

    IMP.ENH: Add multilayer flat chamber impedance calculation.

--- pycolleff/pycolleff/impedances.py
+++ pycolleff/pycolleff/impedances.py
@@ -2,6 +2,7 @@
 
 import os as _os
 import time as _time
+from functools import partial as _partial
 
 import numpy as _np
 import matplotlib.pyplot as _plt
@@ -10,6 +11,7 @@ from scipy.special import kve as _kve, ive as _ive, kv as _kv, iv as _iv, \
     hyp1f1 as _hyp1f1, gamma as _gamma, airy as _airy, airye as _airye
 from scipy.interpolate import PchipInterpolator as _Pchip, \
     CubicSpline as _Spline
+import scipy.integrate as _scyint
 import mpmath as _mpmath
 from mpmath import besseli as _miv, besselk as _mkv, mpc as _mpc, mpf as _mpf
 
@@ -190,7 +192,7 @@ class Budget(list):
         super().__setitem__(k, v)
 
     def __setattr__(self, name, value):
-        if name in {'name','path'}:
+        if name in {'name', 'path'}:
             self.__dict__[name] = str(value)
         else:
             raise AttributeError('Attribute '+name+' is read only.')
@@ -786,6 +788,207 @@ def resistive_multilayer_round_pipe_multiprecision(
     return Zl.conj(), Zv.conj(), Zh.conj()
 
 
+def _calc_matrix_m_prec(kx, beta, epr, mur, nu, b):
+    for p, b_ in enumerate(b):  # length(b) = # de camadas - 1
+        Mt = _mpmath.matrix(4, 4)
+
+        kyp = _mpmath.sqrt(kx*kx + nu[p]*nu[p])
+        kyp1 = _mpmath.sqrt(kx*kx + nu[p+1]*nu[p+1])
+
+        exp_nn = _mpmath.exp(-(kyp + kyp1)*b_)
+        exp_pn = _mpmath.exp((kyp - kyp1)*b_)
+        try:
+            exp_pp = 1/exp_nn
+            exp_np = 1/exp_pn
+        except ZeroDivisionError:
+            return Mt
+
+        nu_ri2 = nu[p+1]/nu[p]
+        nu_ri2 *= nu_ri2
+        ky_r = kyp/kyp1
+        mu_r = mur[p]/mur[p+1]
+        ep_r = epr[p]/epr[p+1]
+
+        pre = ky_r*nu_ri2/2
+        fac = pre*ep_r
+        Mt[0, 0] = (0.5+fac)*exp_pn
+        Mt[0, 1] = (0.5-fac)*exp_nn
+        Mt[1, 0] = (0.5-fac)*exp_pp
+        Mt[1, 1] = (0.5+fac)*exp_np
+
+        fac = kx*(nu_ri2 - 1)/(2*beta*kyp1*epr[p+1])
+        Mt[0, 2] = -fac*exp_pn
+        Mt[0, 3] = -fac*exp_nn
+        Mt[1, 2] = fac*exp_pp
+        Mt[1, 3] = fac*exp_np
+
+        fac = epr[p+1]/mur[p+1]
+        Mt[2, 0] = fac*Mt[0, 2]
+        Mt[2, 1] = fac*Mt[0, 3]
+        Mt[3, 0] = fac*Mt[1, 2]
+        Mt[3, 1] = fac*Mt[1, 3]
+
+        fac = pre*mu_r
+        Mt[2, 2] = (0.5+fac)*exp_pn
+        Mt[2, 3] = (0.5-fac)*exp_nn
+        Mt[3, 2] = (0.5-fac)*exp_pp
+        Mt[3, 3] = (0.5+fac)*exp_np
+
+        if not p:
+            M = Mt
+        else:
+            M = Mt*M
+    return M
+
+
+def _eta_xi_funcs_prec(v, beta, epru, muru, nuu, bu, eprd, murd, nud, bd):
+    epru = [_mpmath.mpc(epi) for epi in epru]
+    muru = [_mpmath.mpc(mui) for mui in muru]
+    nuu = [_mpmath.mpc(nui) for nui in nuu]
+    bu = [_mpmath.mpf(bi) for bi in bu]
+    eprd = [_mpmath.mpc(epi) for epi in eprd]
+    murd = [_mpmath.mpc(mui) for mui in murd]
+    nud = [_mpmath.mpc(nui) for nui in nud]
+    bd = [_mpmath.mpf(bi) for bi in bd]
+    v = _mpmath.mpf(v)
+    beta = _mpmath.mpf(beta)
+
+    matmp = _calc_matrix_m_prec(v, beta, epru, muru, nuu, bu)
+    matmn = _calc_matrix_m_prec(v, beta, eprd, murd, nud, bd)
+    matp = _mpmath.zeros(4, 4)
+    matp[:2, :] = matmp[::2, :]
+    matp[2:, :] = matmn[1::2, :]
+    try:
+        pinv = matp**-1
+    except ZeroDivisionError:
+        print(v, 'zero')
+        return [_mpmath.mpc(0) for _ in range(4)]
+
+    xi1 = pinv[0, 0]*matmp[0, 1] + pinv[0, 1] * matmp[2, 1]
+    xi2 = pinv[1, 0]*matmp[0, 1] + pinv[1, 1] * matmp[2, 1]
+    eta1 = pinv[0, 2]*matmn[1, 0] + pinv[0, 3] * matmn[3, 0]
+    eta2 = pinv[1, 2]*matmn[1, 0] + pinv[1, 3] * matmn[3, 0]
+    return xi1, xi2, eta1, eta2
+
+
+def _integrand(u, kovgamma=1, is_in_t=False, **kwrgs):
+    if is_in_t:
+        t, u = u, (1-u)/u
+    u = _mpmath.mpf(u)
+    v = kovgamma * _mpmath.sinh(u)
+    xi1, xi2, eta1, eta2 = _eta_xi_funcs_prec(v, **kwrgs)
+
+    # General expression:
+    #   itgmn = xi1 + (-1)**m*eta1 + (-1)**n*xi2 + (-1)**(m+n)*eta2
+    #   itgmn *= _mpmath.cosh(m*u)*_mpmath.cosh(n*u)
+
+    itg00 = xi1 + eta1 + xi2 + eta2
+    itg02 = itg00*_mpmath.cosh(2*u)
+
+    itg11 = xi1 - eta1 - xi2 + eta2
+    itg11 *= _mpmath.cosh(u)**2
+
+    itg00 = complex(itg00)
+    itg11 = complex(itg11)
+    itg02 = complex(itg02)
+    itgs = _np.array([
+        itg00.real, itg00.imag,
+        itg11.real, itg11.imag,
+        itg02.real, itg02.imag])
+    if is_in_t:
+        itgs /= t*t
+    return itgs
+
+
+def calc_integrand(u, w, E, *args, is_in_t=False, prec=70):
+    _mpmath.mp.dps = prec
+    gamma = E/E0
+    beta = _np.sqrt(1 - 1/gamma**2)
+    k = w/(beta*_c)
+
+    epr_up, mur_up, b_up, epr_dn, mur_dn, b_dn = args
+    kovgamma = k/gamma
+    nu_up = _np.abs(k)*_np.sqrt(1 - beta**2*epr_up*mur_up)
+    nu_dn = _np.abs(k)*_np.sqrt(1 - beta**2*epr_dn*mur_dn)
+
+    kwrgs = dict(
+        kovgamma=kovgamma, beta=beta, is_in_t=is_in_t,
+        epru=epr_up, muru=mur_up, nuu=nu_up, bu=b_up,
+        eprd=epr_dn, murd=mur_dn, nud=nu_dn, bd=b_dn)
+
+    alpha00 = _np.zeros(u.shape, dtype=complex)
+    alpha11 = _np.zeros(u.shape, dtype=complex)
+    alpha02 = _np.zeros(u.shape, dtype=complex)
+    for i, ui in enumerate(u):
+        alphas = _integrand(ui, **kwrgs)
+        alpha00[i] = alphas[0] + 1j*alphas[1]
+        alpha11[i] = alphas[2] + 1j*alphas[3]
+        alpha02[i] = alphas[4] + 1j*alphas[5]
+
+    return alpha00, alpha11, alpha02
+
+
+def calc_alphas(k, gamma, beta, *args, is_in_t=True):
+    epr_up, mur_up, b_up, epr_dn, mur_dn, b_dn = args
+    kovgamma = k/gamma
+    nu_up = _np.abs(k)*_np.sqrt(1 - beta**2*epr_up*mur_up)
+    nu_dn = _np.abs(k)*_np.sqrt(1 - beta**2*epr_dn*mur_dn)
+
+    supl = _np.inf
+    if is_in_t:
+        supl = 1
+    alpha00 = _np.zeros(k.shape, dtype=complex)
+    alpha11 = _np.zeros(k.shape, dtype=complex)
+    alpha02 = _np.zeros(k.shape, dtype=complex)
+    for i, kovgi in enumerate(kovgamma):
+        t0_ = _time.time()
+        kwrgs = dict(
+            is_in_t=is_in_t, kovgamma=kovgi, beta=beta,
+            epru=epr_up[:, i], muru=mur_up[:, i], nuu=nu_up[:, i], bu=b_up,
+            eprd=epr_dn[:, i], murd=mur_dn[:, i], nud=nu_dn[:, i], bd=b_dn)
+
+        alphas, err, info = _scyint.quad_vec(
+            _partial(_integrand, **kwrgs),
+            0, supl, epsabs=1e-6, norm='max', epsrel=1e-4, full_output=True,
+            workers=-1)
+        alpha00[i], alpha11[i], alpha02[i] = alphas[::2] + 1j*alphas[1::2]
+        print(
+            f'{i:04d}/{len(k):04d} -> '
+            f'freq = {k[i]*beta*_c/2/_np.pi:8.2g} Hz,   '
+            f'converged = {"no " if info.status else "yes":s},   '
+            f'n_evals = {info.neval:04d} '
+            f' (ET: {_time.time()-t0_:.2f} s)')
+
+    return alpha00, alpha11, alpha02
+
+
+def resistive_multilayer_flat_pipe(
+        w, L, E, epr_up, mur_up, b_up, epr_dn, mur_dn, b_dn,
+        prec=70, is_in_t=True):
+    _mpmath.mp.dps = prec
+
+    gamma = E/E0
+    beta = _np.sqrt(1 - 1/gamma**2)
+    k = w/(beta*_c)
+
+    args = epr_up, mur_up, b_up, epr_dn, mur_dn, b_dn
+    alpha00, alpha11, alpha02 = calc_alphas(
+        k, gamma, beta, *args, is_in_t=is_in_t)
+
+    fac = 1j*k * _Z0 * L / (2*_np.pi * beta*gamma**2)
+    Zll = fac * alpha00
+    # Zmy = fac / gamma * alpha01
+
+    fac *= k / gamma**2
+    Zdy = fac * alpha11
+    fac /= 2
+    Zqy = fac * (alpha00 + alpha02)
+    Zqx = fac * (alpha00 - alpha02)
+    Zdx = -Zqx.copy()
+
+    return Zll.conj(), Zdx.conj(), Zdy.conj(), Zqx.conj(), Zqy.conj()
+
+
 def kicker_coupled_flux(w, h, W, t, L, mur, Zg):
     # Calculates Impedances for a ferrite kicker:
     #   - For the Coupled Flux, it uses Davino-Hahn model.
@@ -1157,6 +1360,8 @@ def from_impedance_to_wake(
     if w.min() < 0:
         raise ValueError('All angular frequencies must be positive.')
 
+    # I neet to take the conjugate of the impedance here, because the convention adopted
+    # by ref[1] is different than the one we consider here.
     Z = _np.array(Z.conj(), dtype=_np.complex256)
 
     if interp_type.lower().startswith('spline'):
