1eaf8330265c71ba6cdb5e5ed64fc8c77c56c971
murilobalves <alvesb.murilo@gmail.com>
2023-03-13 17:03:40 -0300

    COLLEF.ENH: Add ALS-U parameters

--- pycolleff/pycolleff/__init__.py
+++ pycolleff/pycolleff/__init__.py
@@ -4,9 +4,9 @@ from . import impedances
 from . import process_wakes
 from . import sirius
 from . import maxiv
+from . import als
 from . import echo2d_util
 from . import tracking
-
 
 import os as _os
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
