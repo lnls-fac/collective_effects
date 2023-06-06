d4a08464b09d5b2d4f0a9323281c89f81d1e173a
murilobalves <alvesb.murilo@gmail.com>
2023-06-06 15:18:00 -0300

    PYCOL.MNT: Delete moved files and fix imports

--- pycolleff/pycolleff/__init__.py
+++ pycolleff/pycolleff/__init__.py
@@ -4,7 +4,8 @@ from . import impedances
 from . import process_wakes
 from . import rings
 from . import echo2d_util
-from . import tracking
+from . import longitudinal_equilibrium
+from . import longitudinal_tracking
 
 import os as _os
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
