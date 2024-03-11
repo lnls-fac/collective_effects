99a2bf778180f18ae1499a84fa0eaefecd2ed293
murilobalves <alvesb.murilo@gmail.com>
2024-03-11 11:53:51 -0300

    RING.FMT: Format ALS ring definition

--- pycolleff/pycolleff/rings/als.py
+++ pycolleff/pycolleff/rings/als.py
@@ -1,23 +1,32 @@
 """."""
 
 import numpy as _np
+
 from ..colleff import Ring as _Ring
 from ..longitudinal_equilibrium import ImpedanceSource as _ImpSource
 
 
 def create_ring():
-    """Create ALS-U collective effects model ring based on data provided in [1, 2].
+    """Create ALS-U collective effects model ring.
+
+    Based on data provided in [1, 2].
 
     Returns:
         ring (pycolleff.colleff.Ring): main parameters for collective effects
 
     References:
-    [1] Warnock, R. (2021). Equilibrium of an arbitrary bunch train with cavity resonators and short range wake: Enhanced iterative solution with Anderson acceleration. Physical Review Accelerators and Beams, 24 (10), 104402. https://doi.org/10.1103/physrevaccelbeams.24.104402
+    [1] Warnock, R. (2021). Equilibrium of an arbitrary bunch train with
+    cavity resonators and short range wake: Enhanced iterative solution
+    with Anderson acceleration. Physical Review Accelerators and Beams,
+    24 (10), 104402. https://doi.org/10.1103/physrevaccelbeams.24.104402
 
-    [2] Warnock, R. (2021). Equilibrium of an arbitrary bunch train in the presence of multiple resonator wakefields. Physical Review Accelerators and Beams, 24 (2), 1–27. https://doi.org/10.1103/PhysRevAccelBeams.24.024401
+    [2] Warnock, R. (2021). Equilibrium of an arbitrary bunch train in
+    the presence of multiple resonator wakefields. Physical Review
+    Accelerators and Beams, 24 (2), 1–27.
+    https://doi.org/10.1103/PhysRevAccelBeams.24.024401
     """
     ring = _Ring()
-    ring.version = 'ALS-U'
+    ring.version = "ALS-U"
     ring.rf_freq = 500.390e6
     ring.mom_comp = 2.025e-4  # momentum compaction factor
     ring.energy = 2e9  # energy [eV]
@@ -45,9 +54,9 @@ def create_harmonic_cavity():
     cav.Q = 2.4e4
     cav.harm_rf = 3
     freq_rf = 500.390e6
-    cav.ang_freq_rf = 2*_np.pi*freq_rf
+    cav.ang_freq_rf = 2 * _np.pi * freq_rf
     detune_f = 317.80e3
-    cav.ang_freq = 2*_np.pi*(3*freq_rf + detune_f)
+    cav.ang_freq = 2 * _np.pi * (3 * freq_rf + detune_f)
 
     # # Low R/Q
     # cav.shunt_impedance = 1.4e6
@@ -61,6 +70,7 @@ def create_harmonic_cavity():
 
 
 def create_main_cavity():
+    """."""
     # Main cavities parameters provided in [2]
 
     # cav = _ImpSource()
@@ -78,13 +88,13 @@ def create_main_cavity():
     beta = 9.983  # optimum
     # beta = 3.1  # ALS heritage
     Rs0 = 9.8e6
-    cav.shunt_impedance = Rs0/(1+beta)
+    cav.shunt_impedance = Rs0 / (1 + beta)
     Q0 = 3.6e4
-    cav.Q = Q0/(1+beta)
+    cav.Q = Q0 / (1 + beta)
     cav.harm_rf = 1
     freq_rf = 500.390e6
-    cav.ang_freq_rf = 2*_np.pi*freq_rf
+    cav.ang_freq_rf = 2 * _np.pi * freq_rf
     detune_f = -94.729e3
-    cav.ang_freq = 2*_np.pi*(freq_rf + detune_f)
-    cav.loop_ctrl_ang_freq = 2*_np.pi*freq_rf
+    cav.ang_freq = 2 * _np.pi * (freq_rf + detune_f)
+    cav.loop_ctrl_ang_freq = 2 * _np.pi * freq_rf
     return cav
